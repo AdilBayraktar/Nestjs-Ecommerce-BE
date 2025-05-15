0ed0784f0ac26d5f1da6dff80fad281e6a35718a
AdilBayraktar <adilbayraktar1997@gmail.com>
2025-05-15 20:02:39 +0300

    input validations

--- src/products/dtos/create-product.dto.ts
+++ src/products/dtos/create-product.dto.ts
@@ -1,4 +1,14 @@
+import { IsNotEmpty, IsNumber, IsString, Length, Max, MaxLength, Min, MinLength } from 'class-validator';
+
 export class CreateProductDTO {
+  @IsString({ message: 'Name must be a string' })
+  @IsNotEmpty({ message: 'Name is required' })
+  @Length(3, 150, { message: 'Name must be between 3 and 150 characters' })
   name: string;
+
+  @IsNotEmpty({ message: 'Price is required' })
+  @IsNumber({}, { message: 'Price must be a number' })
+  @Min(0, { message: 'Price must be a positive number' })
+  @Max(100000, { message: 'Price must be at most 100000' })
   price: number;
 }
--- src/products/dtos/update-product.dto.ts
+++ src/products/dtos/update-product.dto.ts
@@ -1,4 +1,16 @@
+import { IsNotEmpty, IsNumber, IsOptional, IsString, Length, Max, Min } from 'class-validator';
+
 export class UpdateProductDTO {
+  @IsString({ message: 'Name must be a string' })
+  @IsNotEmpty({ message: 'Name is required' })
+  @Length(3, 150, { message: 'Name must be between 3 and 150 characters' })
+  @IsOptional()
   name?: string;
+
+  @IsNotEmpty({ message: 'Price is required' })
+  @IsNumber({}, { message: 'Price must be a number' })
+  @Min(0, { message: 'Price must be a positive number' })
+  @Max(100000, { message: 'Price must be at most 100000' })
+  @IsOptional()
   price?: number;
 }
